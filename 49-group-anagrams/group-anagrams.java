958c40175b63c68fa22c908ac76f0905165dc58e
Omprakash Kumar <129845528+opkrchauhan@users.noreply.github.com>
2026-07-03 01:02:56 +0530

    Time: 529 ms (5.01%) | Memory: 49.3 MB (91.93%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        List<List<String>> result = new ArrayList<>();
+        boolean[] visited = new boolean[strs.length];
+
+        for (int i = 0; i < strs.length; i++) {
+
+            if (visited[i]) {
+                continue;
+            }
+
+            List<String> group = new ArrayList<>();
+            group.add(strs[i]);
+            visited[i] = true;
+
+            for (int j = i + 1; j < strs.length; j++) {
+
+                if (!visited[j] && isAnagram(strs[i], strs[j])) {
+                    group.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+
+            result.add(group);
+        }
+
+        return result;
+    }
+
+    private boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] charArr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            charArr[s.charAt(i) - 'a']++;
+            charArr[t.charAt(i) - 'a']--;
+        }
+
+        for (int x : charArr) {
+            if (x != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
