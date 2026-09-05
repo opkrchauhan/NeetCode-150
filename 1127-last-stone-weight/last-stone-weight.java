7f2a1429f80173e4966ab04979b900444652ff97
Omprakash Kumar <129845528+opkrchauhan@users.noreply.github.com>
2026-09-06 00:31:16 +0530

    Time: 2 ms (31.09%) | Memory: 43.1 MB (15.21%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer>maxHeap = new PriorityQueue<>((a,b)->b-a);
+
+        for(int x : stones){
+            maxHeap.add(x);
+        }
+
+        while(maxHeap.size() > 1){
+            int y = maxHeap.poll();
+            int x = maxHeap.poll();
+
+            if(x!=y){
+                maxHeap.add(y-x);
+            }
+        }
+       
+       return maxHeap.isEmpty()?0:maxHeap.poll();
+
+    }
+}
